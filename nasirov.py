6f0bd190318628de272abbe7ffa50c4b9de53106
blackbelt888 <153537552+blackbelt888@users.noreply.github.com>
2024-01-21 19:40:51 -0600

    test

--- nasirov.py
+++ nasirov.py
@@ -2,7 +2,8 @@ import streamlit as st
 
 import pandas as pd
 import plotly.express as px
-
+import warnings
+warnings.filterwarnings('ignore')
 
 path = 'Housing Price Dubai UAE.csv'
 df=pd.read_csv(path)
@@ -11,7 +12,8 @@ st.write(df)
 
 # Streamlit app
 st.title("Real Estate Data Visualization")
-
+st.title('test')
+st.title('ziya')
 # Scatter Plot for Price and Size
 scatter_fig = px.scatter(df, x='size_in_sqft', y='price', color='neighborhood', size='no_of_bedrooms', hover_data=['no_of_bathrooms'])
 st.plotly_chart(scatter_fig)
@@ -23,4 +25,6 @@ st.plotly_chart(histogram_fig)
 # Box Plot for Price per Square Foot by Neighborhood
 box_fig = px.box(df, x='neighborhood', y='price_per_sqft', points='all', title='Price per Square Foot by Neighborhood')
 st.plotly_chart(box_fig)
+
+
 
